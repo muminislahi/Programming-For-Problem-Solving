c93ba9a440032e0cd9354bb1537d7b490c765e8d
Mumin Islahi <islahimumin@gmail.com>
2025-04-21 20:35:51 +0530

    Create quotient_of_twonums.c

---
+++ chap1/quotient_of_twonums.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float a,b;
+    printf("\nInput the dividend\n");
+    scanf("%f",&a);
+    printf("\nInput the divisor\n");
+    scanf("%f",&b);
+    printf("\nThe quotient when the two numbers are divided is %f",a/b);
+
+  return 0;
+}

144ba7a55b3984c989f4fab06359786c98e22b44
Mumin Islahi <islahimumin@gmail.com>
2025-04-21 20:38:57 +0530

    Create remainder_of_twonums.c

---
+++ chap1/remainder_of_twonums.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    printf("\nInput the dividend\n");
+    scanf("%d",&a);
+    printf("\nInput the divisor\n");
+    scanf("%d",&b);
+    printf("\nThe remainder when the two numbers are divided is %d",a%b);
+
+  return 0;
+}

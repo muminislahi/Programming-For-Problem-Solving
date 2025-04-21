9be803fc4b69926a302c8b2f9d41700d356c3d64
Mumin Islahi <islahimumin@gmail.com>
2025-04-21 20:31:32 +0530

    Create swap_twonums.c

---
+++ chap1/swap_twonums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    float w,x,y,z;
+    printf("\nInput the first number\n");
+    scanf("%f",&w);
+    printf("\nInput the next number\n");
+    scanf("%f",&x);
+    y=x;
+    z=w;
+    printf("%f %f",y,z);
+
+
+  return 0;
+}

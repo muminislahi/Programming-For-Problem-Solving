254072bfa1bb9d12f13aa7244473a9029bb54303
Mumin Islahi <islahimumin@gmail.com>
2025-04-21 20:32:50 +0530

    Create product_of_twonums.c

---
+++ chap1/product_of_twonums.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    float x,y;
+    printf("\nInput the first number\n");
+    scanf("%f",&x);
+    printf("\nInput the second number\n");
+    scanf("%f",&y);
+    printf("\nThe product of the two numbers is %f\n",x*y);
+
+
+    return 0;
+}

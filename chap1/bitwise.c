b15273337bf5c6f476323377af156911da3fd80f
Mumin Islahi <islahimumin@gmail.com>
2025-05-22 21:31:54 +0530

    Create bitwise.c

---
+++ chap1/bitwise.c
@@ -0,0 +1,13 @@
+//Usage of bitwise operators
+#include <stdio.h>
+
+int main() {
+
+    int a=123,b=87;
+    printf("a&b is equal to %d\n",a&b );
+    printf("a|b is equal to %d\n", a|b);
+    printf("a^b is equal to %d \n", a^b);
+    printf("~a is equal to %d\n", ~a);//gives us 2's compliment
+
+    return 0;
+}

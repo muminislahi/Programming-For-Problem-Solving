0d08e80852998345d6781fae5aacec80987824c5
Mumin Islahi <islahimumin@gmail.com>
2025-06-04 19:10:20 +0530

    Add files via upload

---
+++ mumin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char var1,var2,var3,var4,var5;
+    var1=77,var2=85,var3=77,var4=73,var5=78;
+    printf("%c",var1);
+    printf("%c",var2);
+    printf("%c",var3);
+    printf("%c",var4);
+    printf("%c",var5);
+    
+    return 0;
+}

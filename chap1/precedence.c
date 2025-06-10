28a99c13ed168b7a261cdcf04bff8fbde31752e1
Mumin Islahi <islahimumin@gmail.com>
2025-06-10 16:07:13 +0530

    Add files via upload

---
+++ chap1/precedence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+int a;
+a=fun1() + fun2();
+printf("%d", a);
+return 0;
+}
+
+int fun1(){
+    printf("Mumin");
+    return 1;
+}
+int fun2(){
+    printf("Islahi");
+    return 1;
+}
+        
+
+        
